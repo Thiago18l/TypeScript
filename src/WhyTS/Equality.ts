7a5801a4752af8906c2f695cfae6b660652bac72
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-03-04 16:40:33 -0300

    Equality in TS

---
+++ src/WhyTS/Equality.ts
@@ -0,0 +1,12 @@
+
+
+console.log(5 =="5"); // in JS is true, but in TS is a error;
+console.log(5 === "5"); // false, TS error;
+
+console.log("" == "0"); // error, false
+console.log(0 == ""); // is true
+
+//
+
+console.log("" === "0") // false
+console.log(0 === ""); // false
