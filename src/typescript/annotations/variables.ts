5e358a2becd27e67e8480168406ec0f43f1b5bdf
Thiago <thiago.lopes.dev@gmail.com>
2020-05-13 02:19:58 -0300

    annotations with variables

---
+++ src/typescript/annotations/variables.ts
@@ -0,0 +1,14 @@
+let apple: number = 5;
+
+apple = 10;
+
+console.log(apple)
+
+
+let speed: string = 'fast';
+let hasName: boolean = true;
+let nothing: null = null;
+let nothingReally: undefined = undefined;
+
+// Objects
+let now: Date = new Date();
