1ec82ac80610f98c6ba85db8e16c8eb6c58cd2a4
Thiago <thiago.lopes.dev@gmail.com>
2020-05-13 02:25:53 -0300

    arrays with ts

--- src/typescript/annotations/variables.ts
+++ src/typescript/annotations/variables.ts
@@ -12,3 +12,16 @@ let nothingReally: undefined = undefined;
 
 // Objects
 let now: Date = new Date();
+
+// Arrays
+let colors: string[] = ['red', 'green', 'blue']
+colors.map((value:string, index: number) => {
+    console.log(`color ${index+1}: ${value}`)
+})
+
+let myNumbers: number[] = [1, 2, 3, 4, 5, 6, 7]
+let truths: boolean[] = [true, false, true, false]
+
+myNumbers.forEach((value: number) => {
+    console.log(`My Numbers ${value}`)
+})
