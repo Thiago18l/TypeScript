7427ecfe5f6bf64211fabbbbddc1171bc35f44a5
Thiago <thiago.lopes.dev@gmail.com>
2020-05-13 03:30:34 -0300

    annotation with functions

---
+++ src/typescript/annotations/functions.ts
@@ -0,0 +1,10 @@
+const add: (a:number, b: number) => number = (a: number, b: number) => {
+    return a + b;
+}
+console.log(add(2, 2))
+
+const mult = (a: number, b: number): number => {
+    return a * b;
+}
+
+console.log(mult(4, 4))
