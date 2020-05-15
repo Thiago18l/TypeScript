3c732733c389990fe508e7402c5d8c0d2815bac2
Thiago <thiago.lopes.dev@gmail.com>
2020-05-15 01:28:49 -0300

    arrays in ts

---
+++ src/typescript/Arrays/arrays.ts
@@ -0,0 +1,10 @@
+
+const carMakers: string[] = ['ford', 'toyota', 'chevrolet'];
+const dates: Date[] = [new Date(), new Date()]
+
+
+const carsByMake: string[][] = [
+    ['f150'],
+    ['corolla'],
+    ['onix']
+]
