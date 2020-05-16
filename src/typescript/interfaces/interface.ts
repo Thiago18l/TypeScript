bc983b26258ac4df490a51127240e506a497d1aa
Thiago <thiago.lopes.dev@gmail.com>
2020-05-15 21:28:13 -0300

    summary function on interface

--- src/typescript/interfaces/interface.ts
+++ src/typescript/interfaces/interface.ts
@@ -2,16 +2,23 @@ const oldCivic = {
     name: 'civic',
     year: 2000,
     broken: true,
+    sumary(): string {
+        return `Name: ${this.name}, 
+                Year: ${this.year}, 
+                Broken: ${this.broken}`
+    }
 }
 interface Vehicle {
     name: string;
     year: number;
     broken: boolean;
+    sumary(): string;
 }
 
 const printVehicle = (vehicle: Vehicle): void => {
     console.log(`${vehicle.name}`)
     console.log(`${vehicle.year}`)
     console.log(`${vehicle.broken}`)
+    console.log(vehicle.sumary())
 }
 printVehicle(oldCivic)
