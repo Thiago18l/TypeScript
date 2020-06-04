b808d456720906397c9b29fc3e84d589bcf2b96f
Thiago <thiago.lopes.dev@gmail.com>
2020-06-04 01:51:08 -0300

    add new feat about class

--- src/typescript/classes/classes.ts
+++ src/typescript/classes/classes.ts
@@ -9,3 +9,24 @@ class Vehicle {
 const vehicle = new Vehicle();
 vehicle.drive()
 vehicle.beep()
+
+class Car extends Vehicle {
+    private name: string;
+    private placa: string;
+    constructor(name: string, placa: string) {
+        super();
+        this.name = name;
+        this.placa = placa;
+    }
+    getName(): string {
+        return this.name;
+    }
+    getPlaca(): string {
+        return this.placa;
+    }
+}
+
+const carro = new Car("Corolla", "77777");
+carro.beep()
+console.log(carro.getName())
+console.log(carro.getPlaca())
